18382d86a48d7a714afa4e71819634242717d059
imyungchu <angie20920@gmail.com>
2024-04-21 20:10:25 +0800

    add the test data and present the data on the webpage.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+	import carsData from './data.json'
+	import { ref } from 'vue'
+	
+	const cars = ref( carsData )
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<h1>Our Cars</h1>
+		<div>
+			<div v-for='car in cars' :key='car.id'>
+				<h1>{{ car.make }}</h1>
+				<p>${{ car.price }}</p>
+			</div>
+		</div>
+	</main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
