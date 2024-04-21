162f27eac81731fcc31b5b2069c5597ff8631b71
imyungchu <angie20920@gmail.com>
2024-04-21 20:22:53 +0800

    Update the nav bar with the route link. Add the active class on nav bar.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <script setup>
-  import {RouterView} from 'vue-router'
+	import {RouterView, RouterLink} from 'vue-router'
 </script>
 
 <template>
-  <main>
-    <div class='links'>
-      <a href='/'>Home</a>
-      <a href='/about'>About</a>
-    </div>
-    <RouterView />
-  </main>
+	<main>
+		<div class='links'>
+			<RouterLink active-class='active' to='/'>Home</RouterLink>
+			<RouterLink active-class='active' to='/about'>About</RouterLink>
+		</div>
+		<RouterView />
+	</main>
 </template>
 
 <style scoped>
-  .links {
-    padding: 20px
-  }
-  .links a {
-    margin: 0 5px
-  }
+	.links {
+		padding: 20px
+	}
+	.links a {
+		margin: 0 5px
+	}
+	.active {
+		font-weight: bold;
+		color: green;
+	}
 </style>
