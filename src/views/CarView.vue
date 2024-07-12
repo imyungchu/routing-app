56da50f2e3c4293365fcb71769c65aa00f458cf5
imyungchu <angie20920@gmail.com>
2024-07-12 08:46:57 +0800

    feat: add router to each card

---
+++ src/views/CarView.vue
@@ -0,0 +1,29 @@
+<script setup>
+	import {useRoute, useRouter} from 'vue-router'
+	import {ref, onBeforeMount} from "vue"
+	import cars from "../data.json"
+
+	const router = useRouter()
+	const car = ref(null)
+	const route = useRoute()
+	const {id} = route.params;
+	onBeforeMount(() => {
+		car.value = cars.find(c => c.id === parseInt(id))
+	})
+</script>
+
+<template>
+	<div class='container'>
+		<div v-if="car">
+			<h1>The Car</h1>
+			<p>Make: {{car.make}}</p>
+			<p>Body: {{car.body}}</p>
+			<p>Price: {{car.price}}</p>
+			<p>Year: {{car.year}}</p>
+			<button @click="router.back()">Go Back</button>
+		</div>
+		<div v-else>
+			<h1>Car Not Found</h1>
+		</div>
+	</div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
   import carsData from '../data.json'
   import {ref} from 'vue'
+	import {useRouter} from "vue-router"
 
+	const router = useRouter()
   const cars = ref(carsData)
 </script>
 
@@ -9,7 +11,7 @@
   <main class='container'>
     <h1>Our Cars</h1>
     <div class='cards'>
-      <div v-for='car in cars' :key='car.id' class='card'>
+      <div v-for='car in cars' :key='car.id' class='card' @click="router.push(`/car/${car.id}`)">
         <h1>{{ car.make }}</h1>
         <p>${{ car.price }}</p>
       </div>
@@ -26,7 +28,7 @@
     justify-content: center;
   }
   .card {
-    box-shadow: 1px 1px 10px rgba(152, 152, 152, 0.394);
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.207);
     padding: 15px;
     width: 150px;
     margin-right: 15px;
